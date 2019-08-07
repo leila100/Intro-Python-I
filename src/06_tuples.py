c0c4946f4a8620e3b64e672473ed89be4b8e0a59
Leila <nb.leila10@gmail.com>
2019-08-07 12:25:16 -0500

    Add answer to 06_tuples

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -16,13 +16,16 @@ automatically.
 # Example:
 
 import math
+from collections import Iterable
+
 
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
-    
+
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
@@ -31,10 +34,17 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+def print_tuple(t):
+    # prints all the values in a tuple
+    if isinstance(t, Iterable):
+        for value in t:
+            print(value)
+    else:
+        print(t)
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
